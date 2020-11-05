5f4704bb3acfeabe5cdc030ad176aa011db1dc98
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-11-05 22:18:54 +0100

    Base formula

---
+++ FormulaRace/FormulaRace/Driver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FormulaRace
+{
+    public class Driver
+    {
+        private string name;
+        private string lastname;
+        private int number;
+
+        public Driver(string name, string, int number)
+        {
+            this.name = name;
+            this.lastname = lastname;
+            this.number = number;
+        }
+
+        public string Name
+        {
+            get { return this.lastname; }
+        }
+
+        public string Lastname
+        {
+            get { return this.lastname; }
+        }
+
+        public int Number
+        {
+            get { return this.number; }
+        }
+    }
+}
