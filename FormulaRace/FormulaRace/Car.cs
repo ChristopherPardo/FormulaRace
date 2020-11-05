761a26c2bccb9969d0f557cfb561d07c1e2085ad
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-11-05 22:28:54 +0100

    create Car.cs

---
+++ FormulaRace/FormulaRace/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaCrash
+{
+    public class Car
+    {
+        private string brand;
+        private string powerUnit;
+
+        public Car(string brand, string powerUnit)
+        {
+        }
+        public string Brand
+        { 
+        }
+        public string PowerUnit
+        {
+
+        }
+
+    }
+}
