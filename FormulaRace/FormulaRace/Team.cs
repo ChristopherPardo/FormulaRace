03c78975197ee145a377ead0bac3395e1d9f2bb9
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-11-05 22:30:49 +0100

    Create public methods

--- FormulaRace/FormulaRace/Team.cs
+++ FormulaRace/FormulaRace/Team.cs
@@ -10,5 +10,45 @@ namespace FormulaRace
         private string teamBase;
         private string teamChief;
         private int woldChampionship;
+
+        public Team(string fullTeamName, string teamBase, string teamChief, int woldChampionship)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FullTeamName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public string TeamBase
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string TeamChief
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public int WoldChampionship
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
