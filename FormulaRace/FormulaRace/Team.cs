3485c2905b0fb341a538e4f44bbba78fb30966ca
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-11-05 22:30:23 +0100

    Create private atributes

---
+++ FormulaRace/FormulaRace/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaRace
+{
+    class Team
+    {
+        private string fullTeamName;
+        private string teamBase;
+        private string teamChief;
+        private int woldChampionship;
+    }
+}
